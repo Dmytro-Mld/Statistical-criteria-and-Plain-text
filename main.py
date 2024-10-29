0b3873ac329250b0035d837cd2407c45c4f4507f
serhii <sergvoly-ipt24@lll.kpi.ua>
2024-10-29 20:27:07 +0200

    Create main file for lab

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from collections import defaultdict
+
+# ukr_alpha = "АБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯабвгдеєжзиіїйклмнопрстуфхцчшщьюя"
+ukr_alpha_lower = "абвгдеєжзиіїйклмнопрстуфхцчшщьюя"
+
+
+
+
+def main(epsilon = pow(10, -12), precision = 12):
+    pass
+
+if __name__ == "__main__":
+    main()
