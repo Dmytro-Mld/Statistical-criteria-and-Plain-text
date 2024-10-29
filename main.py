3fc4532f180594a642026276133f774358224747
serhii <sergvoly-ipt24@lll.kpi.ua>
2024-10-29 20:27:31 +0200

    Create encode and decode functions

--- main.py
+++ main.py
@@ -5,7 +5,25 @@ from collections import defaultdict
 ukr_alpha_lower = "абвгдеєжзиіїйклмнопрстуфхцчшщьюя"
 
 
+encode_helper = {}
+for i, l in enumerate(ukr_alpha_lower):
+    encode_helper[l] = i
 
+# Return array of indexes of leters in a given text
+def encode(text):
+    result = []
+    for l in text:
+        result.append(encode_helper[l])
+
+    return result
+
+# Return leter at index
+def decode(indexes):
+    result = ""
+    for i in indexes:
+        result += ukr_alpha_lower[i]
+
+    return result
 
 def main(epsilon = pow(10, -12), precision = 12):
     pass
