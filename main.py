c4540f9501309051e2a0af1076068510a2095325
serhii <sergvoly-ipt24@lll.kpi.ua>
2024-10-29 20:28:15 +0200

    Create letters_frequency function

--- main.py
+++ main.py
@@ -25,8 +25,34 @@ def decode(indexes):
 
     return result
 
+def letters_frequency(file_path):
+    frequency = defaultdict(int)
+
+    text_length = 0
+    with open(file_path, mode="rt", encoding="utf-8") as file:
+        for line in file:
+            text_length += len(line)
+            for c in line:
+                frequency[c] += 1
+    
+    for k in frequency:
+        frequency[k] = frequency[k] / text_length
+
+    return frequency
+
 def main(epsilon = pow(10, -12), precision = 12):
-    pass
+    file_path = "text_preparation/out_text.txt"
+    
+    fletters = letters_frequency(file_path=file_path)
+    for k in fletters:
+        print(f"{k}: {fletters[k]:0.{precision}f}")
+
+    sum = 0
+    for k in fletters:
+        sum += fletters[k]
+
+    # small test for: SUM f_i ~ 1.0
+    if sum < 1.0 - epsilon or sum > 1.0 + epsilon: raise ValueError(f"sum of all letters frequency should be equal to 1.0, but it is: {sum}")
 
 if __name__ == "__main__":
     main()
